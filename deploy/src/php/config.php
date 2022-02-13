be6d2389f99da0c94921bd83092f2ddea7596d26
Tim Goll <github@timgoll.de>
2022-02-13 14:03:40 +0100

    fork ready to use

--- deploy/src/php/config.php
+++ deploy/src/php/config.php
@@ -3,14 +3,14 @@
         "raw_base" => "https://raw.githubusercontent.com/",
         "core" => [
             "owner" => "TimGoll",
-            "repository" => "github-portfolio",
-            "default_branch" => "master"
+            "repository" => "TimGollDE",
+            "default_branch" => "main"
         ],
         "bio" => [
             "owner" => "TimGoll",
             "repository" => "TimGoll",
             "default_branch" => "main"
         ],
-        "api_key" => "xxx"
+        "api_key" => ""
     ];
 ?>
